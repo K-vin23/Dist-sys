d36b253662dccaf3d8e5cc3a42e766ac1ea72325
K-vin23 <kvin23@DESKTOP-C54U3M0>
2023-10-21 22:31:59 -0500

    firewall update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Dist-sys
+
+<span>![</span><span>Arquitectura del proyecto final</span><span>]</span><span>(</span><span>https://raw.githubusercontent.com/K-vin23/Dist-sys/master/Architecture/Architecture%20dom-almacen.png</span><span>)</span>
+
+|backend API's |Docker image|
+|---|---|
+|Firewall |api-firewall|
+|payments/orders/Deliveries |openjdk|
+|Catalogue |Nodejs|
+|Python |python|
+|Databases |postgres|
+|Gateway |kong/kong-gateway|
+|Message Broker |RabbitMQ|
+
+
 A rp for the Projects of Distributed Systems course in the UCC
--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,10 @@ terraform {
 
 provider "docker" {}
 
+resource "docker_network" "application_network" {
+  name = "application_network"
+}
+
 resource "docker_image" "springboot_image_pay" {
     name = "springboot-pay"
 
@@ -46,6 +50,10 @@ resource "docker_container" "springboot_pay" {
     internal = 8081
     external = 8081
   }
+
+  networks_advanced {
+    name = docker_network.application_network.name
+  }
 }
 
 resource "docker_container" "springboot_ord" {
@@ -55,6 +63,10 @@ resource "docker_container" "springboot_ord" {
     internal = 8090
     external = 8090
   }
+
+  networks_advanced {
+    name = docker_network.application_network.name
+  }
 }
 
 resource "docker_container" "springboot_dom" {
@@ -63,6 +75,10 @@ resource "docker_container" "springboot_dom" {
   ports {
     internal = 8091
     external = 8091
+  }
+
+  networks_advanced {
+    name = docker_network.application_network.name
   }
 }
 
@@ -81,7 +97,11 @@ resource "docker_container" "nodejs" {
   name  = "nodeapi"
   ports {
     internal = 8080
-    external = 8080
+    external = 3000
+  }
+
+  networks_advanced {
+    name = docker_network.application_network.name
   }
 }
 
@@ -98,6 +118,10 @@ resource "docker_image" "python_image" {
 resource "docker_container" "python" {
   image = docker_image.python_image.image_id
   name  = "pythontest"
+
+  networks_advanced {
+    name = docker_network.application_network.name
+  }
 }
 
 resource "docker_image" "rabbitmq_image" {
@@ -110,6 +134,10 @@ resource "docker_container" "rabbitmq" {
   name  = "rabbitservice"
   hostname = "myrabbit"
   env = ["RABBITMQ_DEFAULT_USER=user", "RABBITMQ_DEFAULT_PASS=password"]
+
+  networks_advanced {
+    name = docker_network.application_network.name
+  }
 }
 
 resource "docker_image" "kong_image" {
@@ -133,6 +161,10 @@ resource "docker_container" "kong" {
       external = 8444
     }
     env = ["KONG_DATABASE=off", "KONG_PROXY_ACCESS_LOG=/dev/stdout", "KONG_ADMIN_ACCESS_LOG=/dev/stdout", "KONG_PROXY_ERROR_LOG=/dev/stderr", "KONG_ADMIN_ERROR_LOG=/dev/stderr", "KONG_ADMIN_LISTEN=0.0.0.0:8000, 0.0.0.0:8444 ssl"]
+
+    networks_advanced {
+    name = docker_network.application_network.name
+  }
 }
 
 resource "docker_image" "postgres_image" {
@@ -154,5 +186,48 @@ resource "docker_container" "postgresql" {
     }
 
   env = ["POSTGRES_PASSWORD=0002"]
+}
+
+resource "docker_image" "image_firewall" {
+  name = "api-firewall:latest"
+}
+
+resource "docker_container" "firewall" {
+  name  = "firewall"
+  image = docker_image.image_firewall.image_id
+  
+  restart = "on-failure"
+
+  env = [
+    "APIFW_URL=http://0.0.0.0:8080",
+    "APIFW_API_SPECS=/opt/resources/httpbin.json",
+    "APIFW_SERVER_URL=http://backend:80",
+    "APIFW_SERVER_MAX_CONNS_PER_HOST=512",
+    "APIFW_SERVER_READ_TIMEOUT=5s",
+    "APIFW_SERVER_WRITE_TIMEOUT=5s",
+    "APIFW_SERVER_DIAL_TIMEOUT=200ms",
+    "APIFW_REQUEST_VALIDATION=BLOCK",
+    "APIFW_RESPONSE_VALIDATION=BLOCK",
+    "APIFW_DENYLIST_TOKENS_FILE=/opt/resources/tokens.denylist.db",
+    "APIFW_DENYLIST_TOKENS_COOKIE_NAME=test",
+    "APIFW_DENYLIST_TOKENS_HEADER_NAME=",
+    "APIFW_DENYLIST_TOKENS_TRIM_BEARER_PREFIX=true"
+  ]
+
+  ports {
+    internal = 8080
+    external = 8080
+  }
+
+  volumes {
+    container_path  = "/opt/resources"
+    host_path      = "/volumes/api-firewall"
+    read_only      = true
+  }
+
+  networks_advanced {
+    name = docker_network.application_network.name
+  }
+  
 }
 
